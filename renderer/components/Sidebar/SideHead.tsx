f0dcc3408c1de30dc8d5167c31debc81e501e347
Syncxv <aruldevarackal1@gmail.com>
2022-04-12 19:16:40 +0530

    ok now it does someting

--- renderer/components/Sidebar/SideHead.tsx
+++ renderer/components/Sidebar/SideHead.tsx
@@ -6,9 +6,11 @@ interface Props {}
 const SideHead: React.FC<Props> = () => {
     return (
         <div className="WHAT flex gap-2 p-2 bg-steam-700">
-            <div className="flex items-center justify-start pl-4 bg-gradient-to-br from-steam-600/50 to-steam-500/50 cursor-pointer rounded-sm transition-all duration-200  flex-1 hover:text-gray-100 hover:bg-steam-500">
-                <Link href="/library">Home</Link>
-            </div>
+            <Link href="/library">
+                <div className="flex items-center justify-start pl-4 bg-gradient-to-br from-steam-600/50 to-steam-500/50 cursor-pointer rounded-sm transition-all duration-200  flex-1 hover:text-gray-100 hover:bg-steam-500">
+                    Home
+                </div>
+            </Link>
             <div className="flex items-center justify-center bg-gradient-to-br from-steam-600/50 to-steam-500/50 cursor-pointer rounded-sm transition-all duration-200 hover:text-gray-100 hover:bg-steam-500">
                 <SquaresFour size={32} />
             </div>
